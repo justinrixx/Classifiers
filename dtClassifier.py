7822163587209fcdbc19ee97223d9c7331749b9b
Justin Ricks <justin.rixx@gmail.com>
2016-01-27 08:35:37 -0700

    Add a build_tree method. Not sure if it works . . .

--- dtClassifier.py
+++ dtClassifier.py
@@ -1,3 +1,4 @@
+import copy
 import numpy as np
 
 
@@ -68,12 +69,66 @@ def calculate_information_gain(s, f):
     return loss
 
 
+def get_all_classes(data, feature):
+    classes = set()
+
+    for instance in data:
+        classes.add(instance)
+
+
+def build_tree(data, features):
+    # check if all the targets are the same
+    targets = data[-1]
+    test = np.full_like(targets, targets[0])
+    if np.array_equal(targets, test):
+        return targets[0]
+
+    # check if there are any more features to test
+    elif len(features) == 0:
+
+        # choose the most common class
+        unique, pos = np.unique(targets, return_inverse=True)
+        counts = np.bincount(pos)
+        maxpos = counts.argmax()
+
+        return unique[maxpos]
+
+    # calculate the information gain (or rather loss)
+    else:
+        losses = np.zeros(len(features))
+        for feature in range(len(features)):
+            losses[feature] = calculate_information_gain(data, feature)
+
+        # go with the smallest
+        i_smallest = np.argmin(losses)
+        all_values = set(data[:, i_smallest])  # get all possible values of this feature
+
+        tree = {features[i_smallest] : {}}
+
+        # get the data and recurse
+        for value in all_values:
+            # get the parameters ready to pass down
+            new_data = [x for x in data if x[i_smallest] == value]
+            new_features = copy.deepcopy(features)
+            new_features.remove(i_smallest)
+
+            # recurse down the subtree
+            subtree = build_tree(new_data, new_features)
+
+            # once done, add the subtree into the main tree
+            tree[features[i_smallest]][value] = subtree
+
+        # return once the subtrees are all in
+        return tree
+
+
 class DTreeClassifier:
     """A decision tree classifier"""
 
     def __init__(self):
         self.data = ""
         self.targets = ""
+        self.tree = ""
 
     def fit(self, data, targets):
         self.train(data, targets)
@@ -81,6 +136,14 @@ class DTreeClassifier:
     def train(self, data, targets):
         self.data = data
         self.targets = targets
+
+        # zip the data and targets together
+        datatargets = np.concatenate((data, targets), axis=1)
+        features = []
+        for i in range(self.data.shape[1]):
+            features.append(i)
+
+        self.tree = build_tree(datatargets, features)
 
 
     def predict(self, data):
